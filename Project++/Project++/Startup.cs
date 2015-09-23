bd292f130c023b6ed460bd7b033eacde9c292f49
Colin Eyster <cjegy9@mst.edu>
2015-09-23 18:44:20 -0500

    initial commit

---
+++ Project++/Project++/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Project__.Startup))]
+namespace Project__
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
