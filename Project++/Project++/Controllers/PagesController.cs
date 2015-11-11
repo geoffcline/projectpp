f8312dbeaafd487a02b87409c3e74c7b9d91251f
Jordan Williams <jtw0213@gmail.com>
2015-11-11 11:45:50 -0600

    Added Authentication Class to Controllers. Added Shared files page and began design.

--- Project++/Project++/Controllers/PagesController.cs
+++ Project++/Project++/Controllers/PagesController.cs
@@ -26,5 +26,9 @@ namespace Project__.Controllers
         {
             return View();
         }
+        public ActionResult DriveFiles()
+        {
+            return View();
+        }
     }
 }
