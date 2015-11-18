07f064edb6f0bd2a6ad0cc76ddc258ac7b1f2f45
Geoffrey Cline <gdcwd5@mail.mst.edu>
2015-11-18 17:08:12 -0600

    commiting changes

--- Project++/Project++/Controllers/PagesController.cs
+++ Project++/Project++/Controllers/PagesController.cs
@@ -26,5 +26,10 @@ namespace Project__.Controllers
         {
             return View();
         }
+
+        public ActionResult geoff1()
+        {
+            return View();
+        }
     }
 }
